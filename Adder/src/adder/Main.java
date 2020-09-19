b14ad2834a90e5695dfa6c25b09f23c5166c4693
Sophia Boisvert <sophia.m.boisvert@gmail.com>
2020-09-19 17:50:44 -0500

    Modified Main.java to handle an arbitrary amount of inputs

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,16 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide valid integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        int sum = 0;
+	for(int i = 0; i < args.length; i++)
+	{
+		sum += Integer.valuesOf(args[0]);
+	}
+	return sum;
     }
 }
