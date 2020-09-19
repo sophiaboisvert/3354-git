17ef0059c3a5b0b89bb3e60b1ec45c836f245399
Sophia Boisvert <sophia.m.boisvert@gmail.com>
2020-09-19 18:16:31 -0500

    Subject: Modified errors in Main.java

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,15 +7,15 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide valid integers to add");
+            System.err.println("Please enter valid integers");
         }
     }
 
     private static int addArguments(String[] args) {
-        int sum = 0;
+	int sum = 0;
 	for(int i = 0; i < args.length; i++)
 	{
-		sum += Integer.valuesOf(args[0]);
+		sum += Integer.valueOf(args[i]);
 	}
 	return sum;
     }
